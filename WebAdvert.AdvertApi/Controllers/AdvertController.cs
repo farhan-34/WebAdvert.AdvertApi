4bedcbeaeb90c61ef7ad7298748b3935e9c87b50
faaran-34 <m.farhan134@gmail.com>
2022-07-25 14:49:12 +0500

    api added

---
+++ WebAdvert.AdvertApi/Controllers/AdvertController.cs
@@ -0,0 +1,65 @@
+﻿using AdvertApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAdvert.AdvertApi.Services;
+
+namespace WebAdvert.AdvertApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AdvertController : Controller
+    {
+        private readonly IAdvertStorageService _advertStorageService;
+
+        public AdvertController(IAdvertStorageService advertStorageService)
+        {
+            _advertStorageService = advertStorageService;
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(201, Type=typeof(CreateAdvertResponse))]
+        public async Task<IActionResult> Create(AdvertModel model)
+        {
+            string recordId;
+            try
+            {
+                recordId = await _advertStorageService.Add(model);
+            }
+            catch (KeyNotFoundException exception)
+            {
+                return new NotFoundResult();
+            }
+            catch (Exception exception)
+            {
+                return StatusCode(500, exception.Message);
+                throw;
+            }
+
+            return StatusCode(201, new CreateAdvertResponse { Id = recordId});
+        }
+
+
+        [HttpPut]
+        [Route("Confirm")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200)]
+        public async Task<IActionResult> Confirm(ConfirmAdvertModel model)
+        {
+            try
+            {
+                await _advertStorageService.Confirm(model);
+            }
+            catch (KeyNotFoundException)
+            {
+                return new NotFoundResult();
+            }
+            catch(Exception exception)
+            {
+                return StatusCode(500, exception.Message);
+            }
+            return new OkResult();
+        }
+    }
+}
---
+++ WebAdvert.AdvertApi/Services/AdvertDBModel.cs
@@ -0,0 +1,23 @@
+﻿using AdvertApi.Models;
+using Amazon.DynamoDBv2.DataModel;
+
+namespace WebAdvert.AdvertApi.Services
+{
+    [DynamoDBTable("Advert")]
+    public class AdvertDBModel
+    {
+        [DynamoDBHashKey]
+        public string Id { get; set; }
+        [DynamoDBProperty]
+        public string Title { get; set; }
+        [DynamoDBProperty]
+        public string Description { get; set; }
+        [DynamoDBProperty]
+        public double Price { get; set; }
+        [DynamoDBProperty]
+        public DateTime CreationDateTime { get; set; }
+        [DynamoDBProperty]
+        public AdvertStatus Status { get; set; }
+
+    }
+}
